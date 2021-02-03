da72eb6498912d5c3c51da910004cd3f007ede99
yanataranets <yana.taranet.s@gmail.com>
2021-02-03 11:39:50 +0100

    main page

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,6 +18,8 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+//    public const MAIN = '/main';
+
 
     /**
      * The controller namespace for the application.
